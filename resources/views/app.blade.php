619bfcf19294e596cb1307b04e5db45e0b5c2272
gacoprotect <gacoprotect@gmail.com>
2025-06-16 06:48:39 +0700

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,10 +40,23 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    @routes
-    @if (app()->environment('local'))
+
+    @php
+    $isProduction = app()->environment('production');
+    $manifestPath = $isProduction ? '../public_html/build/manifest.json' : public_path('build/manifest.json');
+    @endphp
+
+    @if ($isProduction && file_exists($manifestPath))
+    @php
+    $manifest = json_decode(file_get_contents($manifestPath), true);
+    @endphp
+    <link rel="stylesheet" href="{{ config('app.url') }}/build/{{ $manifest['resources/css/app.css']['file'] }}">
+    <script type="module" src="{{ config('app.url') }}/build/{{ $manifest['resources/js/app.js']['file'] }}"></script>
+    @else
     @viteReactRefresh
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     @endif
+    @routes
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
